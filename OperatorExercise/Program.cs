79d79cb6659e4797501037885cb94bd043f1ee07
Levi Joyce <linuxgamer82@gmail.com>
2024-04-29 15:44:55 -0400

    completed operation program

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,40 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello, World!");
+
+            int a = 17;
+            int b = 4;
+            int addition = a + b;
+            int subtraction = a - b;
+            int multiplication = a * b;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a} + {b} is {addition}\n{a} - {b} is {subtraction}\n{a} * {b} is {multiplication}\n{a} / {b} is {quotient} remainder of {remainder}");
+
+            Console.WriteLine();
+
+            Console.WriteLine("What is the Area of your circle?");
+
+            Console.WriteLine();  
+
+            double radius = double.Parse( Console.ReadLine() );
+
+            double area = AreaOfCircle( radius );
+
+            Console.WriteLine($"The area of circle with radius of {radius} is  {area}" );
+
+            Console.WriteLine("The thought exercise answer should be 20");
+            
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            radius = Math.PI * Math.Pow(radius, 2);
+
+            return radius;
         }
     }
 }
